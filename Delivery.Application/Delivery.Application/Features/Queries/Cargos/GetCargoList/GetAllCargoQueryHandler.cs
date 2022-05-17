51e71fbff65b2339eae1525d16360cae9c0cbccd
ErdiDemir <erdi.demir@icloud.com>
2022-05-17 21:19:54 +0300

    Cargo getall added

--- Delivery.Application/Delivery.Application/Features/Queries/Cargos/GetCargoList/GetAllCargoQueryHandler.cs
+++ Delivery.Application/Delivery.Application/Features/Queries/Cargos/GetCargoList/GetAllCargoQueryHandler.cs
@@ -1,25 +1,31 @@
-﻿using System;
+﻿using AutoMapper;
+using Delivery.Application.Features.Queries.Commons.GetAll;
+using Delivery.Application.Models.Cargos;
+using Delivery.Application.Models.Commons;
+using Delivery.Application.Services.Cargos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Delivery.Application.Features.Queries.Cargos.GetCargoList
 {
-    //public class GetAllCargoQueryHandler : GetAllQueryHandler<FoodViewModel>
-    //{
-    //    private readonly IFoodRepository _foodRepository;
+    public class GetAllCargoQueryHandler : GetAllQueryHandler<CargoViewModel>
+    {
+        private readonly ICargoService _cargoService;
 
-    //    public FoodGetAllQueryHandler(IMapper mapper, IFoodRepository foodRepository) : base(mapper)
-    //    {
-    //        _foodRepository = foodRepository;
-    //    }
+        public GetAllCargoQueryHandler(IMapper mapper, ICargoService cargoService) : base(mapper)
+        {
+            _cargoService = cargoService;
+        }
 
-    //    public override async Task<ResponseDto<IReadOnlyList<FoodViewModel>>> Handle(GetAllQuery<FoodViewModel> request, CancellationToken cancellationToken)
-    //    {
-    //        _baseEntities = await _foodRepository.GetAll();
-    //        return await base.Handle(request, cancellationToken);
-    //    }
-    //}
+        public override async Task<ResponseViewModelBase<IReadOnlyList<CargoViewModel>>> Handle(GetAllQuery<CargoViewModel> request, CancellationToken cancellationToken)
+        {
+            _baseEntities = await _cargoService.GetAllAsync();
+            return await base.Handle(request, cancellationToken);
+        }
+    }
 
 }
