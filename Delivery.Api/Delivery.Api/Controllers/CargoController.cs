e567f3ebebf07e3ea433400c2b82bcf29470bb62
ErdiDemir <erdi.demir@icloud.com>
2022-05-28 23:37:32 +0300

    Role Bazında yetkilendirme eklendi

--- Delivery.Api/Delivery.Api/Controllers/CargoController.cs
+++ Delivery.Api/Delivery.Api/Controllers/CargoController.cs
@@ -3,11 +3,13 @@ using Delivery.Application.Models.Cargos;
 using Delivery.Application.Models.Commons;
 using Delivery.Domain.Entities.Cargos;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Delivery.Api.Controllers
 {
+    [Authorize(Roles = "Customer, Admin ")]
     public class CargoController : CustomControllerBase<CargoViewModel, AddCargoCommand>
     {
         public CargoController(IMediator mediator) : base(mediator)
